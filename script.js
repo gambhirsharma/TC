516583c79987addc40cad0dc9e1956eeca9938df
Gambhir ⚡ <gambhir786191ss@gmail.com>
2023-09-12 03:40:34 +0530

    add: loader

--- script.js
+++ script.js
@@ -162,3 +162,20 @@ let tn  = document.querySelectorAll('.TN');
     tn[index].style.display = "none";
    })
  })
+
+
+
+//  loader
+document.onreadystatechange = function() {
+  if (document.readyState !== "complete") {
+      document.querySelector(
+        "body").style.visibility = "hidden";
+      document.querySelector(
+        "#spinner").style.visibility = "visible";
+  } else {
+      document.querySelector(
+        "#spinner").style.display = "none";
+      document.querySelector(
+        "body").style.visibility = "visible";
+  }
+};
